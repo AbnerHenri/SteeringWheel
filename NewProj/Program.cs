5bf6a686c193c6a39f106535dc6ce597c09acf46
Abner Henrique <82474024+AbnerHenri@users.noreply.github.com>
2023-03-21 14:42:53 -0300

    SteeringWheel

---
+++ NewProj/Program.cs
@@ -0,0 +1,36 @@
+﻿
+static void Direction(string dir)
+{
+    switch (dir)
+    {
+        case "Cima":
+            Console.WriteLine("Seguindo pra cima");
+            break;
+        case "Baixo":
+            Console.WriteLine("Seguindo para baixo");
+            break;
+        case "Direita":
+            Console.WriteLine("Virando a direita");
+            break;
+        case "Esquerda":
+            Console.WriteLine("Virando a esquerda");
+            break;
+    }
+}
+
+bool Status = true;
+Console.WriteLine("Digite Quit para sair do console!");
+
+while (Status)
+{
+    Console.Write("Digite a direção : ");
+    string Dir = Console.ReadLine();
+
+    if(Dir == "Quit") {
+        Status = false;
+        Environment.Exit(0);
+    }
+    Direction(Dir);
+}
+
+Console.ReadKey();
